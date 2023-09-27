f55c368db7569f488a1c5d1734cb4a3804e9fb76
Matt <matthew.hansen9@gmail.com>
2023-09-27 20:59:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ $(".nav_link--tt2").on("mouseleave", function () {
 // Check for initial page load
 $(document).ready(function() {
   if($(".section.is-photo-bg").offset().top === 0) {
-    $(".navbar_component.is-top").addClass("sm0.2");
+    $(".navbar_wrap").addClass("sm0.2");
   }
 });
 
@@ -68,7 +68,7 @@ $(".section.is-photo-bg").each(function (index) {
     end: "bottom top",
     markers: false,
     onEnter: () => {
-      $(".navbar").addClass("sm0.2");
+      $(".nav_wrap").addClass("sm0.2");
     },
     onLeave: () => {
       $(".nav_wrap").removeClass("sm0.2");
@@ -82,7 +82,7 @@ $(".section.is-photo-bg").each(function (index) {
   });
 });
 
-
+//For light sections .is-light
 $(".section.is-light").each(function (index) {
   ScrollTrigger.create({
     trigger: $(this),
@@ -94,7 +94,7 @@ $(".section.is-light").each(function (index) {
       $(".nav_wrap_bottom").removeClass("nav_transform--bg0");
     },
     onEnterBack: () => {
-      $(".nav_wrap,.nav_wrap_bottom").addClass(".nav_transition--sm0.1-bg0");
+      $(".nav_wrap,.nav_wrap_bottom").addClass("nav_transition--sm0.1-bg0");
       $(".nav_wrap_bottom").removeClass("nav_transform--bg0");
     }
   });
@@ -218,4 +218,72 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
   // Avoid flash of unstyled content
   gsap.set("[text-split]", { opacity: 1 });
+});
+
+//Swiper JS
+$(".slider_main_component").each(function (index) {
+  let loopMode = false;
+  if ($(this).attr("loop-mode") === "true") {
+    loopMode = true;
+  }
+  let sliderDuration = 300;
+  if ($(this).attr("slider-duration") !== undefined) {
+    sliderDuration = +$(this).attr("slider-duration");
+  }
+  const swiper = new Swiper($(this).find(".swiper")[0], {
+    speed: sliderDuration,
+    loop: loopMode,
+    autoHeight: false,
+    centeredSlides: loopMode,
+    followFinger: true,
+    freeMode: false,
+    slideToClickedSlide: false,
+    slidesPerView: 1,
+    spaceBetween: "4%",
+    rewind: false,
+    mousewheel: {
+      forceToAxis: true
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true
+    },
+    breakpoints: {
+      // mobile landscape
+      480: {
+        slidesPerView: 1,
+        spaceBetween: "4%"
+      },
+      // tablet
+      768: {
+        slidesPerView: 2,
+        spaceBetween: "4%"
+      },
+      // desktop
+      992: {
+        slidesPerView: 3,
+        spaceBetween: "2%"
+      }
+    },
+    pagination: {
+      el: $(this).find(".swiper-bullet-wrapper")[0],
+      bulletActiveClass: "is-active",
+      bulletClass: "swiper-bullet",
+      bulletElement: "button",
+      clickable: true
+    },
+    navigation: {
+      nextEl: $(this).find(".swiper-next")[0],
+      prevEl: $(this).find(".swiper-prev")[0],
+      disabledClass: "is-disabled"
+    },
+    scrollbar: {
+      el: $(this).find(".swiper-drag-wrapper")[0],
+      draggable: true,
+      dragClass: "swiper-drag",
+      snapOnRelease: true
+    },
+    slideActiveClass: "is-active",
+    slideDuplicateActiveClass: "is-active"
+  });
 });
